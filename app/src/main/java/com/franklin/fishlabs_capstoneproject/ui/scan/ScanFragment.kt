ac56512cf7ae2b23ac4b6c61c7d0787b9e8f6920
alfanidoelhaq <118499926+alfanidoelhaq@users.noreply.github.com>
2023-12-10 14:43:36 +0700

    first push

---
+++ app/src/main/java/com/franklin/fishlabs_capstoneproject/ui/scan/ScanFragment.kt
@@ -0,0 +1,41 @@
+package com.franklin.fishlabs_capstoneproject.ui.scan
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.franklin.fishlabs_capstoneproject.databinding.FragmentScanBinding
+
+class ScanFragment : Fragment() {
+
+    private var _binding: FragmentScanBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val scanViewModel =
+            ViewModelProvider(this).get(ScanViewModel::class.java)
+
+        _binding = FragmentScanBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+
+
+        
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
